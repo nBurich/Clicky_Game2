fa3bb90081b19063d103e02375cbe5a25537e346
nBurich <n.burich@hotmail.com>
2019-05-11 12:06:23 -0400

    Updates

---
+++ precache-manifest.68d26e63b450011254a6c4581ff1da81.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff9c9e82941bddccbe37",
+    "url": "/Clicky_Game2/static/js/runtime~main.de8acb97.js"
+  },
+  {
+    "revision": "30c024ef71815710942a",
+    "url": "/Clicky_Game2/static/js/main.3e6f574e.chunk.js"
+  },
+  {
+    "revision": "1ad258c8ad11d64bafc3",
+    "url": "/Clicky_Game2/static/js/2.e3afa17f.chunk.js"
+  },
+  {
+    "revision": "4d697d78603951982416fbd2d4f7a975",
+    "url": "/Clicky_Game2/index.html"
+  }
+];
